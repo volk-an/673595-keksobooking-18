27e5c2c41d6ec8147b90355c5b41ef167dc5fa22
volk-an <volk-an@mail.ru>
2019-10-03 00:08:41 +0700

    заполнение по шаблону

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
+// массмвы данных для создания массива объявлений
 var advertisementParms = {
-  //author: {
   AVATARS: ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'],
-//  },
-  //offer: {
-    TITLES: ['двушка в центре', 'вилла с бассейном', 'однокомнатная квартира рядом с метро', 'стильный лофт 18+', 'семейные аппартаменты'],
-    TYPES: ['palace', 'flat', 'house', 'bungalo'],
-    //rooms: number ,//число, количество комнат
-    //guests: number, //число, количество гостей, которое можно разместить
-    CHEKIN_TIMES: ['12:00', '13:00', '14:00'],
-    CHECKOUT_TIMES: ['12:00', '13:00', '14:00'],
-    FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    DESCRIPTIONS: ['Уютная квартирка с ручными привидениями', 'осторожно! злая собака', 'постоянно топят соседи сверху, держите зонт наготове', 'Незабудьте кормить хозяйских драконов утром и вечером. Корм в холодильнике', 'парковочное место для вашего вертолета предоставляется бесплатно'], //строка с описанием,
-    PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-  //},
-  //location: {
-  //  "x": , //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-  //  "y": ,//случайное число, координата y метки на карте от 130 до 630.
-  //}
+  TITLES: ['двушка в центре', 'вилла с бассейном', 'однокомнатная квартира рядом с метро', 'стильный лофт 18+', 'семейные аппартаменты'],
+  TYPES: ['palace', 'flat', 'house', 'bungalo'],
+  TIMES: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  DESCRIPTIONS: ['Уютная квартирка с ручными привидениями', 'осторожно! злая собака', 'постоянно топят соседи сверху, держите зонт наготове', 'Незабудьте кормить хозяйских драконов утром и вечером. Корм в холодильнике', 'парковочное место для вашего вертолета предоставляется бесплатно'], //строка с описанием,
+  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
 };
+//функция выбора элемента из массива
 var getRandomElement = function (elements) {
   return elements[Math.floor((Math.random() * elements.length))];
 };
+//функция генерации случайного числа из диапазона
 var getRandomNumber = function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-var advertisementAmount = 8;
+// данные для создания массива объявлений
+var ADVERT_AMOUNT = 8;
 var MAP_WIDTH = 1200;
 var X = {
   min: 0,
@@ -34,7 +27,10 @@ var Y = {
   min: 130,
   max: 630
 }
-
+var MAX_PRICE = 10000000;
+var ROOM_AMOUNT = 10;
+var GUEST_AMONT = 10;
+//функция создания массива л=объявлений
 var generateAdvertisements = function (advertisementAmount) {
   var advertisements = [];
   for (var i = 0; i < advertisementAmount; i++) {
@@ -47,12 +43,12 @@ var generateAdvertisements = function (advertisementAmount) {
       offer: {
         title: getRandomElement(advertisementParms.TITLES),
         adderss: 'coordX'  +  'coordY',
-        price: getRandomNumber(0, 1000000),
+        price: getRandomNumber(0, MAX_PRICE),
         type: getRandomElement(advertisementParms.TYPES),
-        rooms: getRandomNumber(0, 1000000),
-        guests: getRandomNumber(0, 1000000),
-        checkin: getRandomElement(advertisementParms.CHEKIN_TIMES),
-        checkout: getRandomElement(advertisementParms.CHECKOUT_TIMES),
+        rooms: getRandomNumber(0, ROOM_AMOUNT),
+        guests: getRandomNumber(0, GUEST_AMONT),
+        checkin: getRandomElement(advertisementParms.TIMES),
+        checkout: getRandomElement(advertisementParms.TIMES),
         features: getRandomElement(advertisementParms.FEATURES),
         description: getRandomElement(advertisementParms.DESCRIPTIONS),
         photos: getRandomElement(advertisementParms.PHOTOS),
@@ -66,4 +62,26 @@ var generateAdvertisements = function (advertisementAmount) {
 
 console.log(advertisements);
 };
-generateAdvertisements(advertisementAmount);
+//активируем карту
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+//определяем, куда вставлять новые пины
+var similarAdvertList = document.querySelector('.map__pins');
+//определяем шаблон для пина
+var similarPinTemplate = document.querySelector('#pin');
+//функция заполнения данными из сгенерированного массива по шаблону
+var renderPin = function () {
+  var fragment = document.createDocumentFragment();
+  var adverts = generateAdvertisements(ADVERT_AMOUNT);
+  adverts.forEach(function(advert) {
+    var newPin = similarPinTemplate.cloneNode(true);
+    newPin.querySelector('.map__pin').style.left = advert.location.x;
+    newPin.querySelector('.map__pin').style.top = advert.location.y;
+    newPin.querySelector('img').src = advert.author.avatr;
+    newPin.querySelector('.img').alt = advert.offer.description;
+    fragment.appendChild(newPin);
+  });
+  similarAdvertList.appendChild(fragment);
+}
+
+//generateAdvertisements(ADVERT_AMOUNT);
