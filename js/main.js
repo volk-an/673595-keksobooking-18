2ee01a2f7ed0808a53edc91aa5f429b02e1237f0
volk-an <volk-an@mail.ru>
2019-10-04 00:12:26 +0700

    отрисовывает один пин

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// массмвы данных для создания массива объявлений
+'use strict';
+//  массмвы данных для создания массива объявлений
 var advertisementParms = {
   AVATARS: ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'],
   TITLES: ['двушка в центре', 'вилла с бассейном', 'однокомнатная квартира рядом с метро', 'стильный лофт 18+', 'семейные аппартаменты'],
@@ -8,41 +9,40 @@ var advertisementParms = {
   DESCRIPTIONS: ['Уютная квартирка с ручными привидениями', 'осторожно! злая собака', 'постоянно топят соседи сверху, держите зонт наготове', 'Незабудьте кормить хозяйских драконов утром и вечером. Корм в холодильнике', 'парковочное место для вашего вертолета предоставляется бесплатно'], //строка с описанием,
   PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
 };
-//функция выбора элемента из массива
+//  функция выбора элемента из массива
 var getRandomElement = function (elements) {
   return elements[Math.floor((Math.random() * elements.length))];
 };
-//функция генерации случайного числа из диапазона
+//  функция генерации случайного числа из диапазона
 var getRandomNumber = function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 // данные для создания массива объявлений
-var ADVERT_AMOUNT = 8;
-var MAP_WIDTH = 1200;
-var X = {
-  min: 0,
-  max: MAP_WIDTH
-}
-var Y = {
-  min: 130,
-  max: 630
-}
+var AMOUNT = 8;
+var xCoord = {
+  MIN: 0,
+  MAX: 1200
+};
+var yCoord = {
+  MIN: 130,
+  MAX: 630
+};
 var MAX_PRICE = 10000000;
 var ROOM_AMOUNT = 10;
 var GUEST_AMOUNT = 10;
-//функция создания массива л=объявлений
+//  функция создания массива л=объявлений
 var generateAdvertisements = function (advertisementAmount) {
   var advertisements = [];
   for (var i = 0; i < advertisementAmount; i++) {
-    var coordX = getRandomNumber(X.min, X.max);
-    var coordY = getRandomNumber(Y.min, Y.max);
+    var coordX = getRandomNumber(xCoord.MIN, xCoord.MAX);
+    var coordY = getRandomNumber(yCoord.MIN, yCoord.MAX);
     advertisements[i] = {
       author: {
         avatar: getRandomElement(advertisementParms.AVATARS)
       },
       offer: {
         title: getRandomElement(advertisementParms.TITLES),
-        adderss: 'coordX'  +  'coordY',
+        address: 'coordX' + 'coordY',
         price: getRandomNumber(0, MAX_PRICE),
         type: getRandomElement(advertisementParms.TYPES),
         rooms: getRandomNumber(0, ROOM_AMOUNT),
@@ -53,35 +53,34 @@ var generateAdvertisements = function (advertisementAmount) {
         description: getRandomElement(advertisementParms.DESCRIPTIONS),
         photos: getRandomElement(advertisementParms.PHOTOS),
       },
-    location: {
-      x: coordX,
-      y: coordY
-    }
+      location: {
+        x: coordX,
+        y: coordY
+      }
+
   };
-  };
-  console.log(advertisements)
   return advertisements;
 };
-//активируем карту
+//  активируем карту
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-//определяем, куда вставлять новые пины
+//  определяем, куда вставлять новые пины
 var similarAdvertList = document.querySelector('.map__pins');
-//определяем шаблон для пина
-var similarPinTemplate = document.querySelector('#pin');
-//функция заполнения данными из сгенерированного массива по шаблону
-var renderPin = function () {
+//  определяем шаблон для пина
+var similarPinTemplate = document.querySelector('#pin').content .querySelector('.map__pin');;
+//  функция заполнения данными из сгенерированного массива по шаблону
+var renderPins = function () {
   var fragment = document.createDocumentFragment();
-  var adverts = generateAdvertisements(ADVERT_AMOUNT);
+  var adverts = generateAdvertisements(AMOUNT);
+    console.log(adverts)
   adverts.forEach(function(advert) {
     var newPin = similarPinTemplate.cloneNode(true);
-    newPin.querySelector('.map__pin').style.left = advert.location.x;
-    newPin.querySelector('.map__pin').style.top = advert.location.y;
+    newPin.style.left = advert.location.x;
+    newPin.style.top = advert.location.y;
     newPin.querySelector('img').src = advert.author.avatar;
     newPin.querySelector('img').alt = advert.offer.description;
     fragment.appendChild(newPin);
   });
   similarAdvertList.appendChild(fragment);
 };
-renderPin()
-//generateAdvertisements(ADVERT_AMOUNT);
+renderPins()
